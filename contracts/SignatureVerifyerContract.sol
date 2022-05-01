af8362c8f789ee8ca0f5e38f800c42253ca84150
Réka Fazakas <47921568+Bunface@users.noreply.github.com>
2022-05-01 22:22:00 +0200

    modifier address hash

--- contracts/SignatureVerifyerContract.sol
+++ contracts/SignatureVerifyerContract.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.0;
 pragma solidity <=0.10.0;
 
 contract SignatureVerifyerContract{
+
      function verifySendersAddressSignedWithCacheAddress(
         bytes memory signature,
         address cachePublicKey,
@@ -18,18 +19,19 @@ contract SignatureVerifyerContract{
         return
             keccak256(
                 abi.encodePacked(
-                    "\x19Ethereum Signed Message:\n32", 
-                    keccak256(abi.encodePacked(string(abi.encodePacked(signedAddress)))))
+                    "\x19Ethereum Signed Message:\n32",
+                    keccak256((abi.encodePacked(signedAddress)))
+                )
             );
     }
 
-    function recoverSigner(bytes32 ethSignedMessageHash, bytes memory signature)
+    function recoverSigner(bytes32 _ethSignedMessageHash, bytes memory _signature)
         public
         pure
         returns (address)
     {
-        (bytes32 r, bytes32 s, uint8 v) = splitSignature(signature);
-        return ecrecover(ethSignedMessageHash, v, r, s);
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
+        return ecrecover(_ethSignedMessageHash, v, r, s);
     }
 
     function splitSignature(bytes memory sig)
@@ -42,11 +44,14 @@ contract SignatureVerifyerContract{
         )
     {
         require(sig.length == 65, "invalid signature length");
+
         assembly {
             r := mload(add(sig, 32))
             s := mload(add(sig, 64))
             v := byte(0, mload(add(sig, 96)))
         }
+
+        // implicitly return (r, s, v)
     }
 
 }
